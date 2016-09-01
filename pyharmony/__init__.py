e8277772313c5e81081479fd1c64a76e290f2ad0
iandday <iandday@gmail.com>
2016-08-31 20:22:24 -0400

    Update __init__.py
    allow easier import

--- pyharmony/__init__.py
+++ pyharmony/__init__.py
@@ -2,3 +2,7 @@
 # All rights reserved.
 
 """Module for connecting to and controlling the Logitech Harmony Link"""
+from .__main__ import *
+from .auth import *
+from .client import *
+__all__ = ["__main__","auth","client"]
